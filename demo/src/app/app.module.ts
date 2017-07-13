e5315ddb6da8aa4ccdce8bcb6b9436c6430c0752
Moin Shaikh <shaikhmoin250@gmail.com>
2017-07-13 12:35:38 +0530

    My import files

---
+++ demo/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { MyLoginComponent } from './my-login/my-login.component';
+import { UsersComponent } from './users/users.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyLoginComponent,
+    UsersComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
